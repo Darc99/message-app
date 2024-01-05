aea1e5b836e857d11749de557bd0ab743925c332
Darc99 <lennyyhugoh@gmail.com>
2024-01-05 23:51:20 +0100

    added controllers with endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
